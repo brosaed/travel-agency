960ed732a05a9fa011c36ebaf6b05ae341a46bf0
Saed <saeddev@Saeds-MacBook-Pro.local>
2025-05-22 21:49:03 +0300

    added components with nav-items

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,15 +1,24 @@
 import { Outlet } from "react-router";
+import pkg from "@syncfusion/ej2-react-navigations";
+import { NavItems } from "../../../components";
+
+const { SidebarComponent } = pkg;
 
 const AdminLayout = () => {
+
     return (
         <div className="admin-layout">
             MobileSidebar
-            <aside className="w-full max-w-[270px] hidden
-            lg:block">Sidebar</aside>
+            <aside className="w-full max-w-[270px] hidden lg:block">
+                <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
+            </aside>
             <aside className="children">
                 <Outlet />
             </aside>
         </div>
     )
 }
-export default AdminLayout
+
+export default AdminLayout;
---
+++ components/NavItems.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router';  // Missing import added
+
+const NavItems = () => {
+    return (
+        <section className="nav-items">
+            <Link to='/' className="link-logo">
+                <img
+                    src="/assets/icons/logo.svg"
+                    alt="logo"
+                    className="size-[30px]"
+                />
+                <h1>Tourvisto</h1>
+            </Link>
+        </section>
+    );
+};
+
+export default NavItems;
